b0cf843aafc2fb7d6cfba3616d4fd893301bf18c
Johan <johantokarskij@yahoo.se>
2024-09-09 07:28:28 +0200

    implemented pagination

--- dlt_code/dlt_projektledare.py
+++ dlt_code/dlt_projektledare.py
@@ -11,15 +11,22 @@ def _get_ads(url_for_search, params):
     response.raise_for_status()
     return json.loads(response.content.decode('utf8'))
 
-@dlt.resource(write_disposition="replace")
-def jobsearch_resource(
-    params
-):
+@dlt.resource(write_disposition='replace')
+def jobsearch_resource(params):
     url = 'https://jobsearch.api.jobtechdev.se'
-    url_for_search = f"{url}/search"
+    url_for_search = f'{url}/search'
     
-    for ad in _get_ads(url_for_search, params)["hits"]:
-        yield ad
+    while True:
+        ads_data = _get_ads(url_for_search, params)
+        hits = ads_data.get('hits', [])
+
+        for ad in hits:
+            yield ad
+        
+        if len(hits) < params['limit']:
+            break
+
+        params['offset'] += params['limit']
     
 def run_pipeline(query, table_name):
     pipeline = dlt.pipeline(
@@ -27,15 +34,15 @@ def run_pipeline(query, table_name):
         destination='snowflake',
         dataset_name='staging',
     )    
-    params = {"q": query, "limit": 100}
+    params = {'q': query, 'limit': 100, 'offset': 0}
     load_info = pipeline.run(jobsearch_resource(params=params), table_name=table_name)
     print(load_info)
     
-if __name__ == "__main__":
+if __name__ == '__main__':
     working_directory = Path(__file__).parent
     os.chdir(working_directory)
     
-    query = "projektledare"
-    table_name = "stg_project_leader_job_ads"
+    query = 'projektledare'
+    table_name = 'stg_project_leader_job_ads'
     
     run_pipeline(query, table_name)
