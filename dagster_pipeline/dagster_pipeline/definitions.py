e932202ee368030a3ee0989f8d38d4bc0b9b8532
Johan <johantokarskij@yahoo.se>
2024-09-14 01:59:21 +0200

    restructured code for better readability

--- dagster_pipeline/dagster_pipeline/definitions.py
+++ dagster_pipeline/dagster_pipeline/definitions.py
@@ -3,11 +3,7 @@ from dagster_dbt import DbtCliResource
 from .assets import dbt_projektledare_dbt_assets, compute
 from .project import dbt_projektledare_project
 from .schedules import schedules
-
 from dagster_embedded_elt.dlt import DagsterDltResource
-
-
-
 
 defs = Definitions(
     assets=[compute, dbt_projektledare_dbt_assets],
