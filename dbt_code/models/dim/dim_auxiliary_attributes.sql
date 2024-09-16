08649c3121408396ef88deb8f111b8dfd19a687e
Emma Martini <emma.martini@hotmail.se>
2024-09-16 10:37:11 +0200

    Changed dim tables values to not null

--- dbt_code/models/dim/dim_auxiliary_attributes.sql
+++ dbt_code/models/dim/dim_auxiliary_attributes.sql
@@ -3,8 +3,8 @@ WITH src_auxiliary_attributes AS (
 )
 SELECT
     {{dbt_utils.generate_surrogate_key(['id', 'employer_name'])}} AS auxiliary_attributes_id,
-    experience_required,
-    driving_license,
-    access_to_own_car,
-    webpage_url
+    COALESCE(experience_required, 'Ej specificerad') AS experience_required,
+    COALESCE(driving_license, 'Ej specificerad') AS driving_license,
+    COALESCE(access_to_own_car, 'Ej specificerad') AS access_to_own_car,
+    COALESCE(webpage_url, 'Ej specificerad') AS webpage_url,
 FROM src_auxiliary_attributes
